562ec6564086d5f0d32530905f59efa337ddecd3
keisukE <kano5guitar@gmail.com>
2026-02-15 04:06:09 +0900

    SELECT練習

--- main.go
+++ main.go
@@ -8,10 +8,21 @@ import (
 )
 
 func main() {
-	dsn := "user=keisukekano dbname=todo sslmode=disable"
-	db, err := sql.Open("postgres", dsn)
+	dsn := "user=keisukekano dbname=postgres sslmode=disable" //DB設定
+	db, err := sql.Open("postgres", dsn)                      //DB接続
 	if err != nil {
 		log.Fatal(err)
 	}
+	if err := db.Ping(); err != nil { //接続確認
+		log.Panic(err)
+	}
+	log.Println("DB接続成功")
 	defer db.Close()
+
+	var dbName string
+	row := db.QueryRow("SELECT current_database()") //現在のデータベースを取得する
+	if err := row.Scan(&dbName); err != nil {       //クエリの結果を変数に格納
+		log.Panic(err)
+	}
+	log.Println("現在のデータベース:", dbName)
 }
