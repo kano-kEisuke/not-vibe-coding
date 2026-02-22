9775be4b18d94f954fc40325ebb7083b6b2ccc36
keisukE <kano5guitar@gmail.com>
2026-02-23 03:04:40 +0900

    CRUD動作チェック完了

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
+	"not-vibe-coding/todo"
 
 	_ "github.com/lib/pq"
 )
@@ -25,4 +27,58 @@ func main() {
 		log.Panic(err)
 	}
 	log.Println("現在のデータベース:", dbName)
+
+	//CRUD動作チェック
+	log.Println("CRUD動作チェック")
+	//挿入
+	id1, err := todo.InsertData(db, "インサートできた")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("挿入されたデータのID", id1)
+	//1件取得
+	todotest, err := todo.GetById(db, id1)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("取得したデータ: ID=%d, Title=%s\n", todotest.TodoId, todotest.TodoTitle)
+	//更新
+	err = todo.UpdateData(db, id1, "更新できた")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("更新完了")
+	//2件目挿入
+	var id2 int
+	id2, err = todo.InsertData(db, "インサート2件目")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("挿入されたデータのID:", id2)
+	//全件取得
+	todos, err := todo.GetAll(db)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("全件取得結果")
+	for _, t := range todos {
+		fmt.Printf("  ID=%d, Title=%s, Done=%v\n", t.TodoId, t.TodoTitle, t.TodoDone)
+	}
+	//削除
+	err = todo.DeleteData(db, id1)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = todo.DeleteData(db, id2)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("削除完了")
+
+	//削除後にデータの残留確認
+	todosAfter, err := todo.GetAll(db)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("✓ 削除後のTodoの件数: %d\n", len(todosAfter))
 }
--- todo/repository.go
+++ todo/repository.go
@@ -42,7 +42,7 @@ func GetById(db *sql.DB, id int) (*Todo, error) {
 	return &todo, nil //該当するデータがある場合はTodo構造体のポインタを返す
 }
 
-// データを挿入して挿入したデータのIDを返す関数
+// データを挿入して、挿入したデータのIDを返す関数
 func InsertData(db *sql.DB, title string) (int, error) {
 	var id int
 	row := db.QueryRow("INSERT INTO todo (todo_title,todo_done) VALUES ($1, false) RETURNING todo_id", title)
