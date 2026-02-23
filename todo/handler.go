bcfe4a12421478f59edb48972bed9c47d4ffbe63
keisukE <kano5guitar@gmail.com>
2026-02-23 23:32:41 +0900

    エンドポイント作ってく

---
+++ todo/handler.go
@@ -0,0 +1,16 @@
+package todo
+
+import "net/http"
+
+func Health(w http.ResponseWriter, r *http.Request) { // 引数で「どこに」「何を」書き込むかを指定　メッセンジャー的な
+	// w: クライアントへの返信先（メッセンジャーの配達先）
+	// r: クライアントからのリクエスト情報（何を要求しているか）
+
+	//HTTP/1.1 200 OK みたいなのがヘッダに書き込まれて返る
+	//ブラウザやクライアントが成功したと認識できる
+	w.WriteHeader(http.StatusOK)
+
+	//レスポンスボディにokを書き込んでる
+	// []byte()で文字列をバイナリに変換（HTTPはバイナリでやり取りするため必要）
+	w.Write([]byte("ok"))
+}
