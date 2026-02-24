ba40d73960ac291a84650df3cf671bcadeca98d7
keisukE <kano5guitar@gmail.com>
2026-02-24 23:19:46 +0900

    試しに作った

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -9,6 +9,6 @@ func main() {
 	//http.HandleFunc 「どの」URLで「なに」をするか
 	http.HandleFunc("/health", todo.Health)
 
-	//サーバー立てて8080ポートで待つ　nilはデフォルトルーター使うって意味らしい
+	//サーバー立てて8080ポートで待つ
 	http.ListenAndServe(":8080", nil)
 }
